4a7af093a97d04b1e004f8bb3a0f8a42618ebd0c
Nicolas-alt <a10100105555@gmail.com>
2021-02-27 17:16:42 -0500

    { Add } Login form

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,11 +1,27 @@
-import React from 'react'
+import React from 'react';
+import './login.scss';
 
 const Login = () => {
-    return (
-        <div>
-            Login
-        </div>
-    )
-}
+  return (
+    <section className="section__signIn">
+      <div>
+        <p>Welcome</p>
+        <h3>Sign In</h3>
+      </div>
+      <form>
+        <label for="userName">User name</label>
+        <input type="text" id="userName" placeholder="Enter your user name" />
+        <label for="password">Password</label>
+        <input
+          type="password"
+          id="password"
+          placeholder="Enter your password"
+        />
+        <button>Sign In</button>
+        <p>Don't have an account?</p>
+      </form>
+    </section>
+  );
+};
 
-export default Login
+export default Login;
