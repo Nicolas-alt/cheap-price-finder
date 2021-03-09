c4890f819232a04912ca357650e0aa6a3c0218a8
Nicolas-alt <a10100105555@gmail.com>
2021-03-09 14:51:00 -0500

    Add cdn boxicons

--- src/pages/User/Dashboard/Dashboard.js
+++ src/pages/User/Dashboard/Dashboard.js
@@ -5,11 +5,28 @@ import './dashboard.scss';
 const Dashboard = ({ children }) => {
   return (
     <section className="section__dashboard">
-      <Link to="/user/home">Home</Link>
-      <Link to="/user/profile">Profile</Link>
-      <Link to="/user/chart">Chart</Link>
-      <Link to="/user/about">About</Link>
-      <Link to="/user/search">Search</Link>
+      <aside>
+        <Link to="/user/home">
+          <i className="bx bx-home-heart"></i>
+          Home
+        </Link>
+        <Link to="/user/profile">
+          <i className="bx bx-user"></i>
+          Profile
+        </Link>
+        <Link to="/user/chart">
+          <i className="bx bx-line-chart"></i>
+          Chart
+        </Link>
+        <Link to="/user/about">
+          <i className="bx bx-info-square"></i>
+          About
+        </Link>
+        <Link to="/user/search">
+          <i className="bx bx-search-alt-2"></i>
+          Search
+        </Link>
+      </aside>
       <div>{children}</div>
     </section>
   );
