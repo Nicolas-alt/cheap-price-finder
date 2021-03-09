33bf92352b25f25a06fedf377bc844d1b40f0647
DanielPaezb <dcpaez595@misena.edu.co>
2021-03-09 09:31:01 -0500

    Update {Cards}

---
+++ src/components/card/Card.js
@@ -0,0 +1,21 @@
+import React from "react";
+import '../../components/card/Card.scss';
+
+const Card = () => {
+  return (
+    <section className="card-contain">
+      <div className="card">
+        <div className="avatar-image">
+          <img className="avatar-image" src="https://i.ibb.co/nnGQ9tJ/face1.png" alt=""/>
+        </div>
+        <div className="body-card">
+          <h2 className='Text-colour-name'>Daniel Páez</h2>
+          <p className='Text-colour-proffesion'>Full Stack Developer</p>
+          <p className='Text-colour-place'>Melbourne, AU</p>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Card;
--- src/pages/Landing/Landing.js
+++ src/pages/Landing/Landing.js
@@ -1,43 +1,78 @@
-import React from 'react';
-import './Landing.scss';
-import Chart from '../../assets/svg/chart.svg';
+import React from "react";
+import "./Landing.scss";
+import Chart from "../../assets/svg/chart.svg";
+import Card from "../../components/card/Card";
 
 const Landing = () => {
-    return (
-        <div className='landing-container'>
-            <div className="clip-path-2"></div>
-            <header className='header-container'>
-                <nav className='nav-container'>
-                    <ul className='ul-container'>
-                        <li className='li-container'><a className='a-links-header' href="#">Home</a></li>
-                        <li className='li-container'><a className='a-links-header' href="#">About Us</a></li>
-                        <li className='li-container'><a className='a-links-header' href="#">Products</a></li>
-                        <li className='li-container'><a className='a-links-header' href="#">Contact</a></li>
-                        <li className='li-container bg-purple'><a className='a-links-header ' href="#">Login</a></li>
-                        <li className='li-container bg-purple'><a className='a-links-header' href="#">Sign Up</a></li>
-                    </ul>
-                </nav>
-            </header>
-            <div className="chart">
-                <img src={Chart} alt=""/>
-            </div>
-            <section className="about margen">
-                <div className='about-titles'>
-                    <h3 className='about-title'>Innovate solution for sourcing candidates</h3>
-                    <br/>
-                    <h2 className='about-subtitle'>Help us find the missing puzzle, get reward properly</h2>
-                </div>
-                <form>
-                    <br/>
-                    <input placeholder='Buscar' type="search" />
-                    <i class="fas fa-search"></i>
-                </form>
-                <br/>
-                <h2 className='margen text-muted'>Recent added:</h2>
-            </section>
-            <div className="clip-path"></div>
+  return (
+    <div className="landing-container">
+      <div className="clip-path-2"></div>
+      <header className="header-container">
+        <nav className="nav-container">
+          <ul className="ul-container">
+            <li className="li-container">
+              <a className="a-links-header" href="#">
+                Home
+              </a>
+            </li>
+            <li className="li-container">
+              <a className="a-links-header" href="#">
+                About Us
+              </a>
+            </li>
+            <li className="li-container">
+              <a className="a-links-header" href="#">
+                Products
+              </a>
+            </li>
+            <li className="li-container">
+              <a className="a-links-header" href="#">
+                Contact
+              </a>
+            </li>
+            <li className="li-container bg-purple">
+              <a className="a-links-header " href="#">
+                Login
+              </a>
+            </li>
+            <li className="li-container bg-purple">
+              <a className="a-links-header" href="#">
+                Sign Up
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <div className="chart">
+        <img src={Chart} alt="" />
+      </div>
+      <section className="about margen">
+        <div className="about-titles">
+          <h3 className="about-title">
+            Innovate solution for sourcing candidates
+          </h3>
+          <br />
+          <h2 className="about-subtitle">
+            Help us find the missing puzzle, get reward properly
+          </h2>
         </div>
-    )
-}
+        <form>
+          <br />
+          <input placeholder="Buscar" type="search" />
+          <i class="fas fa-search"></i>
+        </form>
+        <br />
+        <h2 className="margen text-muted">Recent added:</h2>
+      </section>
+      <section className="cards-container">
+        <Card/>
+        <Card/>
+        <Card/>
+      </section>
 
-export default Landing
+      <div className="clip-path"></div>
+    </div>
+  );
+};
+
+export default Landing;
