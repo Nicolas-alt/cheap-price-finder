22f2a6d844de56713424c91616c5d703f0d83130
DanielPaezb <dcpaez595@misena.edu.co>
2021-03-10 12:33:53 -0500

    add{ cards }

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -1,11 +1,36 @@
-import React from 'react'
+import React from "react";
+import "./About.scss";
+import Card from "./Card/Card";
 
 const About = () => {
-    return (
-        <div>
-            About
-        </div>
-    )
-}
+  return (
+    <div className="about___container">
+      <h2 className="about___title">About Project</h2>
+      <div className="about___paragraph">
+        <p className="paragraph">
+          This project is a product searcher, and it has a particular function
+          of filtering so many prices in Internet and to show the user the most
+          low.
+          <br />
+          <br />
+          <hr />
+          <br />
+          This project was developed by two enthusiasts developers, by the way
+          look for us in:
+          <br />
+          <div className="paragraph__icons">
+            <i class="fab fa-github-alt github"></i>
+            <i class="fab fa-twitter twitter"></i>
+          </div>
+        </p>
+      </div>
+      <h2 className="about___title">Colaborators</h2>
+      <div className="cards___container">
+        <Card name="Daniel Páez" skill="Frontend Developer" />
+        <Card name="Nicolás Jimenez" skill="Full Stack Developer" />
+      </div>
+    </div>
+  );
+};
 
-export default About
+export default About;
