7ebb20cbb3b22f503024840e48df802507a60906
Nicolas-alt <a10100105555@gmail.com>
2021-03-12 11:38:44 -0500

    Update prop types

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -4,28 +4,35 @@ import Card from "./Card/Card";
 
 const About = () => {
   return (
-    <div className="about___container">
+    <section className="about___container">
       <h2 className="about___title">About Project</h2>
       <div className="about___paragraph">
         <p className="paragraph">
           This project is a product searcher, and it has a particular function
           of filtering so many prices in Internet and to show the user the most
           low.
-          This project was developed by two enthusiasts developers, by the way
-          look for us in:
           <br />
-          <span className="paragraph__icons">
-            <i className="fab fa-github-alt github"></i>
-            <i className="fab fa-twitter twitter"></i>
-          </span>
+          This project was developed by two enthusiasts developers.
         </p>
       </div>
       <h2 className="about___title">Colaborators</h2>
       <div className="cards___container">
-        <Card name="Daniel Páez" skill="Frontend Developer"/>
-        <Card name="Nicolás Jimenez" skill="Full Stack Developer" />
+        <Card
+          image="https://avatars.githubusercontent.com/u/57303802?s=460&u=a79e7d6d3854104efa335b6f1b9fa95eff18a9bf&v=4"
+          name="Daniel Páez"
+          skill="Frontend Developer"
+          github="https://github.com/DanielPaezb"
+        />
+        <Card
+          image="https://avatars.githubusercontent.com/u/52179095?s=460&u=3c32f626fcbc00f39db5da407a76ce7a0bb3e7f3&v=4"
+          name="Nicolás Jimenez"
+          skill="Front dev"
+          twitter="https://twitter.com/Nicolas35103573"
+          github="https://github.com/Nicolas-alt"
+          web="https://nicolas-alt.vercel.app"
+        />
       </div>
-    </div>
+    </section>
   );
 };
 
--- src/pages/About/Card/Card.js
+++ src/pages/About/Card/Card.js
@@ -3,51 +3,57 @@ import PropTypes from 'prop-types';
 import './Card.scss';
 
 const Card = ({
+  image,
   name,
   skill,
   facebook = undefined,
   twitter = undefined,
   github = undefined,
-  youtube = undefined,
+  web = undefined,
 }) => {
   return (
-    <div className="profile-card">
+    <article className="profile-card">
       <div className="card-header">
-        <div className="card-image">
-          <img src="https://i.ibb.co/Yt75y7B/favicon.png" alt="" />
-        </div>
-        <div className="name">{name}</div>
-        <div className="profession">{skill}</div>
+        <picture className="card-image">
+          <img src={image} alt="" />
+        </picture>
+        <p className="name">{name}</p>
+        <p className="profession">{skill}</p>
         {name && (
           <div className="social-media">
             {facebook && (
-              <a href={facebook}>
+              <a href={facebook} target="_blank" rel="noreferrer">
                 <i className="fab fa-facebook-f"></i>{' '}
               </a>
             )}
             {twitter && (
-              <a href={twitter}>
+              <a href={twitter} target="_blank" rel="noreferrer">
                 <i className="fab fa-twitter"></i>{' '}
               </a>
             )}
             {github && (
-              <a href={github}>
+              <a href={github} target="_blank" rel="noreferrer">
                 <i className="fab fa-github"></i>
               </a>
             )}
-            {youtube && (
-              <a href={youtube}>
-                <i className="fab fa-youtube"></i>
+            {web && (
+              <a href={web} target="_blank" rel="noreferrer">
+                <i className="fas fa-globe"></i>
               </a>
             )}
           </div>
         )}
       </div>
-    </div>
+    </article>
   );
 };
 
+Card.defaultProps = {
+  image: 'https://i.ibb.co/Yt75y7B/favicon.png',
+};
+
 Card.propTypes = {
+  image: PropTypes.string.isRequired,
   name: PropTypes.string.isRequired,
   skill: PropTypes.string.isRequired,
   facebook: PropTypes.string,
