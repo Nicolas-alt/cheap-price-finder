fbe2516922fba6a329fe650c07519216895c7a10
Nicolas-alt <a10100105555@gmail.com>
2021-05-07 17:47:50 -0500

    Add input search

--- src/pages/Results/Results.tsx
+++ src/pages/Results/Results.tsx
@@ -5,6 +5,11 @@ import './results.scss';
 const Results = () => {
   return (
     <section className="col-span-full">
+      <input
+        type="text"
+        className="rounded-lg border-transparent appearance-none border border-gray-300 w-11/12 py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent m-2"
+        placeholder="Search..."
+      />
       <Card />
       <Card />
       <Card />
